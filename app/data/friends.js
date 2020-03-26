5c41f116f64cc733fef3165d9ad76b58acd3b227
Randy Amos <rfamos53@yahoo.com>
2020-03-25 19:59:18 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArray = [
+    {
+        "name": "Marky Ramone",
+        "photo": "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.rockapedia.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fthreshold-1382%2Fpublic%2Fbiography%2Fpaste_1434056195.png%3Fitok%3DAFTz6hs4&imgrefurl=http%3A%2F%2Fwww.rockapedia.com%2Fbiography%2Fmarky-ramone%2Fdiscography-filmography&tbnid=z6E4wMn41gjpkM&vet=12ahUKEwihybuLgLfoAhXCA50JHROnCdoQMygMegUIARDqAQ..i&docid=FMEBGpmY_vNzsM&w=754&h=800&q=marky%20ramone%20png&ved=2ahUKEwihybuLgLfoAhXCA50JHROnCdoQMygMegUIARDqAQ",
+        "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+        "name": "Joey Young",
+        "photo": "https://images.app.goo.gl/ks2TedajX4muLdvTA",
+        "scores": ["4","2","5","1","3","2","2","1","3","2"]
+    },
+    {
+        "name": "Craig Too",
+        "photo": "https://images.app.goo.gl/9bBsxwaG9ZFR2Xfi9",
+        "scores": ["5","2","2","2","4","1","3","2","5","5"]
+    },
+    {
+        "name": "Michelle Burrough",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Michelle_Borromeo_Actor_Headshots_30.jpg/180px-Michelle_Borromeo_Actor_Headshots_30.jpg",
+        "scores": ["3","3","4","2","2","1","3","2", "2","3"]
+    },
+    {
+        "name": "Lamont Johnson",
+        "photo": "https://www.headshotsnyc.com/wp-content/gallery/actor-headshots-old/Commercial-headshot-African-American-actor.jpg",
+        "scores": ["4","3","4","1","5","2","5","3","1","4"]
+    },
+    {
+        "name": "Keug Ghuru",
+        "photo": "https://cdn-ep19.pressidium.com/wp-content/uploads/2018/11/headshot-poses-diptych.jpg",
+        "scores": ["4","4","2","3","2","2","3","2","4","5"]
+    },
+    {
+        "name": "Jay Giron",
+        "photo": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        "scores": ["1","1","1","1","1","1","1","1","1","1"]
+    },
+    {
+        "name": "Zack Pederson",
+        "photo": "https://lh3.googleusercontent.com/proxy/nJQQhheIGKRYQP1F8UFlPtY0aohXt4VOs7W1_eo4g4vzNEUNqDXSO3ztZAlSLacSr9U4dV5P4yluHmtSaofXg0MRS4e_cyfHjnQOgQSGV-bcfB38zxxuzGK3wJw",
+        "scores": ["1","2","5","3","4","5","1","3","2","3"]
+    }
+]
+
+module.exports = friendsArray;
